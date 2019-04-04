7b8a32dd6c32b0534b82e50c59462ef09bf5e426
IngCampos <15151130@mail.ita.mx>
2019-04-04 11:41:14 -0600

    Orden funciones
    Separacion de funciones propuestas por chava y marcos

---
+++ js/funciones_marcos.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var currentTime = new Date();
+    var currentTimezone = currentTime.getTimezoneOffset();
+  currentTimezone = (currentTimezone/60) * -1;
+
+          if (currentTimezone !== 0)
+            {
+              var utc = currentTimezone > 0 ? ' +' : ' ';
+              utc += currentTimezone;
+            }
+      var screenSize = '';
+      if (screen.width) {
+          width = (screen.width) ? screen.width : '';
+          height = (screen.height) ? screen.height : '';
+          screenSize += '' + width + " x " + height;
+      }
+     // var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
+  $("#sisop").text(navigator.platform);
+  $("#versionsisop").text(navigator.userAgent);
+  $("#navegadorop").text(navigator.appCodeName);
+  $("#versionnavegador").text(navigator.appVersion);
+  $("#resoluc").text(screenSize);
+  $("#profund").text(screen.colorDepth);
+  $("#idioma").text(navigator.language);
+  $("#zonhor").text(utc);
+  $("#orien").text();
+  $("#toscr").text();
+  $("#typedis").text();
+});
