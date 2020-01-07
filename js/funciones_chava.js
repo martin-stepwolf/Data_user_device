0b694c5437ab61ce2f5b98f1125075b36e51996e
IngCampos <15151130@mail.ita.mx>
2020-01-07 13:31:43 -0600

    Eliminacion archivos no necesarios
    La pagina solo mostrara informacion sobre el dispositivo, por lo que la interaccion con la BD se quito.
    Se formateo el codigo

--- js/funciones_chava.js
+++ js/funciones_chava.js
@@ -13,10 +13,10 @@
             width = (screen.width) ? screen.width : '';
             height = (screen.height) ? screen.height : '';
             screenSize += '' + width + " x " + height;
-            if(width>height)
-            orientation = "Vertical";
+            if (width > height)
+                orientation = "Vertical";
             else
-            orientation = "Horizontal";
+                orientation = "Horizontal";
         }
 
         // browser
@@ -94,10 +94,10 @@
 
         // device version
         var device = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
-        if(device = "false")
-        device = "Desktop";
+        if (device = "false")
+            device = "Desktop";
         else
-        device = "Mobile";
+            device = "Mobile";
         // cookie
         var cookieEnabled = (navigator.cookieEnabled) ? true : false;
 
@@ -108,33 +108,110 @@
 
         // system
         var os = unknown;
-        var clientStrings = [
-            {s:'Windows 10', r:/(Windows 10.0|Windows NT 10.0)/},
-            {s:'Windows 8.1', r:/(Windows 8.1|Windows NT 6.3)/},
-            {s:'Windows 8', r:/(Windows 8|Windows NT 6.2)/},
-            {s:'Windows 7', r:/(Windows 7|Windows NT 6.1)/},
-            {s:'Windows Vista', r:/Windows NT 6.0/},
-            {s:'Windows Server 2003', r:/Windows NT 5.2/},
-            {s:'Windows XP', r:/(Windows NT 5.1|Windows XP)/},
-            {s:'Windows 2000', r:/(Windows NT 5.0|Windows 2000)/},
-            {s:'Windows ME', r:/(Win 9x 4.90|Windows ME)/},
-            {s:'Windows 98', r:/(Windows 98|Win98)/},
-            {s:'Windows 95', r:/(Windows 95|Win95|Windows_95)/},
-            {s:'Windows NT 4.0', r:/(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
-            {s:'Windows CE', r:/Windows CE/},
-            {s:'Windows 3.11', r:/Win16/},
-            {s:'Android', r:/Android/},
-            {s:'Open BSD', r:/OpenBSD/},
-            {s:'Sun OS', r:/SunOS/},
-            {s:'Linux', r:/(Linux|X11)/},
-            {s:'iOS', r:/(iPhone|iPad|iPod)/},
-            {s:'Mac OS X', r:/Mac OS X/},
-            {s:'Mac OS', r:/(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
-            {s:'QNX', r:/QNX/},
-            {s:'UNIX', r:/UNIX/},
-            {s:'BeOS', r:/BeOS/},
-            {s:'OS/2', r:/OS\/2/},
-            {s:'Search Bot', r:/(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+        var clientStrings = [{
+                s: 'Windows 10',
+                r: /(Windows 10.0|Windows NT 10.0)/
+            },
+            {
+                s: 'Windows 8.1',
+                r: /(Windows 8.1|Windows NT 6.3)/
+            },
+            {
+                s: 'Windows 8',
+                r: /(Windows 8|Windows NT 6.2)/
+            },
+            {
+                s: 'Windows 7',
+                r: /(Windows 7|Windows NT 6.1)/
+            },
+            {
+                s: 'Windows Vista',
+                r: /Windows NT 6.0/
+            },
+            {
+                s: 'Windows Server 2003',
+                r: /Windows NT 5.2/
+            },
+            {
+                s: 'Windows XP',
+                r: /(Windows NT 5.1|Windows XP)/
+            },
+            {
+                s: 'Windows 2000',
+                r: /(Windows NT 5.0|Windows 2000)/
+            },
+            {
+                s: 'Windows ME',
+                r: /(Win 9x 4.90|Windows ME)/
+            },
+            {
+                s: 'Windows 98',
+                r: /(Windows 98|Win98)/
+            },
+            {
+                s: 'Windows 95',
+                r: /(Windows 95|Win95|Windows_95)/
+            },
+            {
+                s: 'Windows NT 4.0',
+                r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/
+            },
+            {
+                s: 'Windows CE',
+                r: /Windows CE/
+            },
+            {
+                s: 'Windows 3.11',
+                r: /Win16/
+            },
+            {
+                s: 'Android',
+                r: /Android/
+            },
+            {
+                s: 'Open BSD',
+                r: /OpenBSD/
+            },
+            {
+                s: 'Sun OS',
+                r: /SunOS/
+            },
+            {
+                s: 'Linux',
+                r: /(Linux|X11)/
+            },
+            {
+                s: 'iOS',
+                r: /(iPhone|iPad|iPod)/
+            },
+            {
+                s: 'Mac OS X',
+                r: /Mac OS X/
+            },
+            {
+                s: 'Mac OS',
+                r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/
+            },
+            {
+                s: 'QNX',
+                r: /QNX/
+            },
+            {
+                s: 'UNIX',
+                r: /UNIX/
+            },
+            {
+                s: 'BeOS',
+                r: /BeOS/
+            },
+            {
+                s: 'OS/2',
+                r: /OS\/2/
+            },
+            {
+                s: 'Search Bot',
+                r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/
+            }
         ];
         for (var id in clientStrings) {
             var cs = clientStrings[id];
@@ -173,8 +250,7 @@
             var fv = swfobject.getFlashPlayerVersion();
             if (fv.major > 0) {
                 flashVersion = fv.major + '.' + fv.minor + ' r' + fv.release;
-            }
-            else  {
+            } else {
                 flashVersion = unknown;
             }
         }
@@ -195,9 +271,9 @@
 }(this));
 
 console.log(
-    'OS: ' + jscd.os +' '+ jscd.osVersion + '\n' +
-    'Browser: ' + jscd.browser +' '+ jscd.browserMajorVersion +
-      ' (' + jscd.browserVersion + ')\n' + 
+    'OS: ' + jscd.os + ' ' + jscd.osVersion + '\n' +
+    'Browser: ' + jscd.browser + ' ' + jscd.browserMajorVersion +
+    ' (' + jscd.browserVersion + ')\n' +
     'device: ' + jscd.device + '\n' +
     'Flash: ' + jscd.flashVersion + '\n' +
     'Cookies: ' + jscd.cookies + '\n' +
--- js/funciones_marcos.js
+++ js/funciones_marcos.js
@@ -1,20 +1,19 @@
-$(document).ready(function() {
-    var currentTime = new Date();
-    var currentTimezone = currentTime.getTimezoneOffset();
-  currentTimezone = (currentTimezone/60) * -1;
+$(document).ready(function () {
+  var currentTime = new Date();
+  var currentTimezone = currentTime.getTimezoneOffset();
+  currentTimezone = (currentTimezone / 60) * -1;
 
-          if (currentTimezone !== 0)
-            {
-              var utc = currentTimezone > 0 ? ' +' : ' ';
-              utc += currentTimezone;
-            }
-      var screenSize = '';
-      if (screen.width) {
-          width = (screen.width) ? screen.width : '';
-          height = (screen.height) ? screen.height : '';
-          screenSize += '' + width + " x " + height;
-      }
-     // var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
+  if (currentTimezone !== 0) {
+    var utc = currentTimezone > 0 ? ' +' : ' ';
+    utc += currentTimezone;
+  }
+  var screenSize = '';
+  if (screen.width) {
+    width = (screen.width) ? screen.width : '';
+    height = (screen.height) ? screen.height : '';
+    screenSize += '' + width + " x " + height;
+  }
+  // var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
   $("#usera").text(navigator.userAgent);
   $("#sisop").text(jscd.os);
   $("#versionsisop").text(jscd.osVersion);
